d44fed32afb7cc0f2df172ba4c3c081b6bd7ea70
Murray Coueslant <murray.coueslant@gmail.com>
2019-09-06 09:25:19 -0400

    started the CLI version from scratch, added command logic and file reading / splitting logic.

---
+++ Illumi_CLI/Illumi_CLI/illumiFileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Illumi_CLI
+{
+    internal class illumiFileReader
+    {
+        internal static string[] ReadFile(FileInfo filePath)
+        {
+            if (filePath.Exists)
+            {
+                string fullFileText = File.ReadAllText(filePath.FullName);
+                string[] programTexts = fullFileText.Split('$');
+                return programTexts;
+            } else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Illumi_CLI/Illumi_CLI/illumiLexer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Illumi_CLI
+{
+    internal class illumiLexer
+    {
+        internal static void Lex(string[] filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
