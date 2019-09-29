4fe8d0bd7df93c36df081249d32d4f8fe39c903c
Murray Coueslant <murray.coueslant@gmail.com>
2019-09-29 01:12:14 -0400

    added some basic scaffolding for the parser, namely the non-terminal parsing methods. tomorrow i shall flesh one, if not more, of them out and start to actually parse and consume tokens

---
+++ Illumi_CLI/src/Parser.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Illumi_CLI
+{
+    public class Parser
+    {
+        public Parser() { }
+
+        public void Parse() { }
+
+        public void ParseProgram() { }
+
+        public void ParseBlock() { }
+
+        public void ParseStatementList() { }
+
+        public void ParseStatement() { }
+
+        public void ParsePrintStatement() { }
+
+        public void ParseAssignmentStatement() { }
+
+        public void ParseVariableDeclaration() { }
+
+        public void ParseWhileStatement() { }
+
+        public void ParseIfStatement() { }
+
+        public void ParseExpression() { }
+
+        public void ParseIntExpression() { }
+
+        public void ParseStringExpression() { }
+
+        public void ParseBooleanExpression() { }
+
+        public void ParseIdentifier() { }
+    }
+}
